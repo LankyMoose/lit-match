686677b7bda182f8887e98b25ce797aae74a5028
LankyMoose <rausten93@gmail.com>
2025-03-16 01:25:04 +1300

    improve internal impl, JSDoc desc/example

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -11,28 +11,45 @@ type Matcher<T, R, Remaining = T> = {
     ? () => R
     : NonExhaustiveError<Remaining>
 }
+
 /**
- * Creates a pattern matching expression for a given value
+ * Creates a "pattern matching"-like  expression for a given value
+ * @see https://www.npmjs.com/package/lit-match
+ * @example
+ * ```ts
+ * const statusText = match(status)
+ *   .with(Status.Idle, () => "Idle")
+ *   .with(Status.Active, () => "Active")
+ *   .with(Status.Error, () => "Error")
+ *   .exhaustive()
+ *
+ * console.log(statusText)
+ * //           ^? string
+ * ```
  */
-export function match<const T>(value: T) {
+export function match<T>(value: T) {
   const handlers: [unknown, Function][] = []
-  const matcher = {
-    with<V extends T, U extends Function>(matchValue: V, handler: U) {
-      return handlers.push([matchValue, handler]), matcher
-    },
-    else<U extends (value: T) => any>(handler: U): never | ReturnType<U> {
-      for (const [pattern, handler] of handlers) {
-        if (value === pattern) return handler(value)
-      }
-      return handler(value)
-    },
-    exhaustive() {
-      for (const [pattern, handler] of handlers) {
-        if (value === pattern) return handler(value)
-      }
-      // If we reach here, no pattern matched, which shouldn't happen with exhaustive matching
-      throw new NonExhaustiveError()
-    },
+  const resolve = (fallback: Function) => {
+    for (const [matchValue, handler] of handlers) {
+      if (value === matchValue) return handler(value)
+    }
+    return fallback(value)
   }
-  return matcher as Matcher<T, never, T>
+
+  return {
+    with(matchValue, handler) {
+      handlers.push([matchValue, handler])
+      return this as Matcher<
+        T,
+        ReturnType<typeof handler>,
+        Exclude<T, typeof matchValue>
+      >
+    },
+    else: resolve,
+    exhaustive() {
+      return resolve(() => {
+        throw new NonExhaustiveError()
+      })
+    },
+  } as Matcher<T, never, T>
 }
